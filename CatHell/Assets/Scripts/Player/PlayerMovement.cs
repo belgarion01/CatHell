c38735ec15decfc64a3acc08cccf4688e1faef55
Hugo Raguin <hugo.raguin@gmail.com>
2022-03-04 20:53:41 +0100

    repushed Shapes and Inptu System

--- CatHell/Assets/Scripts/Player/PlayerMovement.cs
+++ CatHell/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float _movementSpeed = 5f;
+    
+    private float _hAxis;
+    private float _vAxis;
+    private Rigidbody _rb;
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _hAxis = Input.GetAxisRaw("Horizontal");
+        _vAxis = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 translation = new Vector3(_hAxis, 0, _vAxis) * _movementSpeed * Time.fixedDeltaTime;
+        _rb.MovePosition(_rb.position + translation);
     }
 }
